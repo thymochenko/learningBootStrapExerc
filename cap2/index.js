69ea4358b7d31d87700b9e98c93b681bd9b51191
thymochenko <henkosato5@gmail.com>
2016-02-16 18:08:37 -0200

    primeiros exercícios cap 2

---
+++ cap2/index.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var path = __dirname + '/';
+
+var router = express.Router(); 
+router.get("/", function (req, res){
+	res.sendFile(path + "index.html");
+});
+
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
+
